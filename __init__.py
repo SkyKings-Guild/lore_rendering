9cf24a73f42cdb45a22e9cdf24ed6e001bed4b56
plun1331 <plun1331@gmail.com>
2022-10-26 14:29:12 -0700

    fix: return bytes instead of io

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ __all__ = (
 )
 
 
-def sync_render(text):
+def sync_render(text, return_io=True) -> io.BytesIO | bytes:
     to_pass = text.splitlines()
     image, images = raw_render(to_pass)
     saved = io.BytesIO()
@@ -18,9 +18,15 @@ def sync_render(text):
     else:
         image.save(saved, format='gif', append_images=images, save_all=True, duration=75, loop=0)
     saved.seek(0)
+    if not return_io:
+        return saved.read()
     return saved
 
 
-async def render(text, *, loop=None):
+async def render(text, *, loop=None, return_io=True) -> io.BytesIO | bytes:
     loop = loop or asyncio.get_event_loop()
-    return await loop.run_in_executor(None, sync_render, text)
+
+    def wrapper():
+        return sync_render(text, return_io=return_io)
+
+    return await loop.run_in_executor(None, wrapper)
